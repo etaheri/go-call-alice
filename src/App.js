d665c6d8ca212f8a6f1a67db12061cd568610fa2
Erik Taheri <etaheri@delawarenorth.com>
2018-07-17 12:02:28 -0400

    add basic routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+// Components
+import Home from "./Home.js";
+import Map from "./Map.js";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <Router>
+            <div>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                  <Link to="/map">Map</Link>
+                </li>
+              </ul>
+              <hr />
+              <Route exact path="/" component={Home} />
+              <Route path="/map" component={Map} />
+            </div>
+          </Router>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
