0539a8a09cc5215eb69bd233121a4fa3ae3ebf15
Erik Taheri <etaheri@delawarenorth.com>
2018-07-17 12:02:44 -0400

    add basic routing

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class HomePage extends Component {
+  render() {
+    return (
+        <div className="App">
+          <p className="App-intro">Home</p>
+        </div>
+    );
+  }
+}
+
+export default HomePage;
---
+++ src/Map.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class MapPage extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p className="App-intro">Map</p>
+      </div>
+    );
+  }
+}
+
+export default MapPage;
